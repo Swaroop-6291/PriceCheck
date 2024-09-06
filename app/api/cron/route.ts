147b68b415ea16955eda3eb9f6b4e1fa739f6f2b
Swaroop-6291 <swaroop6291@gmail.com>
2024-09-06 21:01:50 +0530

    Fix

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 
 import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/utils";
-import { connectToDB } from "@/lib/mongoose";
+import { connectToDB } from "@/lib/Scrapper/mongoose";
 import Product from "@/lib/models/product.model";
-import { scrapeAmazonProduct } from "@/lib/scraper";
-import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+import { scrapeAmazonProduct } from "@/lib/Scrapper";
+import { generateEmailBody,sendEmail } from "@/lib/Nodemailer";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 59; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
